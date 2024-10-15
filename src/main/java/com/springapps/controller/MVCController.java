c8aa9a038c39f3eec195f1b68c57ccb0fc12d1d6
vinodh1988 <k.c.vinodh@hotmail.com>
2024-10-15 11:53:22 +0530

    controller

--- src/main/java/com/springapps/controller/MVCController.java
+++ src/main/java/com/springapps/controller/MVCController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/web")
 public class MVCController {
 
-	@RequestMapping("home")
+	@RequestMapping("/home")
     public String home() {
 		return "index";
 	} 	
