29d2a492aa29f8a7bc3187e2924a0ccd1c1ba617
vinodh1988 <k.c.vinodh@hotmail.com>
2024-10-15 11:16:12 +0530

    mvc

--- src/main/java/com/springapps/SpringBootCompleteApplication.java
+++ src/main/java/com/springapps/SpringBootCompleteApplication.java
@@ -8,6 +8,7 @@ public class SpringBootCompleteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootCompleteApplication.class, args);
+		System.out.println("App started");
 	}
 
 }
