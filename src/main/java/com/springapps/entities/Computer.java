ef439ea520533192dbe936aa49a9f1627e42396d
vinodh1988 <k.c.vinodh@hotmail.com>
2024-10-15 13:47:14 +0530

    rest-continued

--- src/main/java/com/springapps/entities/Computer.java
+++ src/main/java/com/springapps/entities/Computer.java
@@ -1,5 +1,54 @@
 package com.springapps.entities;
 
 public class Computer {
+   private String brand;
+   private String ram;
+   private String cpu;
+   private String disk;
+   private double price;
+   
+public Computer() {
+	
+}
 
+public Computer(String brand, String ram, String cpu, String disk, double price) {
+	super();
+	this.brand = brand;
+	this.ram = ram;
+	this.cpu = cpu;
+	this.disk = disk;
+	this.price = price;
+}
+public String getBrand() {
+	return brand;
+}
+public void setBrand(String brand) {
+	this.brand = brand;
+}
+public String getRam() {
+	return ram;
+}
+public void setRam(String ram) {
+	this.ram = ram;
+}
+public String getCpu() {
+	return cpu;
+}
+public void setCpu(String cpu) {
+	this.cpu = cpu;
+}
+public String getDisk() {
+	return disk;
+}
+public void setDisk(String disk) {
+	this.disk = disk;
+}
+public double getPrice() {
+	return price;
+}
+public void setPrice(double price) {
+	this.price = price;
+}
+   
+   
 }
--- src/main/java/com/springapps/services/DataService.java
+++ src/main/java/com/springapps/services/DataService.java
@@ -1,11 +1,25 @@
 package com.springapps.services;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.springapps.entities.Computer;
 
 @Service
 public class DataService {
    public String[] getNames() {
 	   String[] names= {"Harry","Nicholas","Gerald","Roger","Vinay","Mahesh"};
 	   return names;
+   }
+   
+   public List<Computer> getComputers() {
+	   List<Computer> list=new ArrayList<Computer>();
+	   list.add(new Computer("Lenovo","16GB","i7","512GB",40403.50));
+	   list.add(new Computer("Dell","16GB","i3","512GB",40402.50));
+	   list.add(new Computer("HP","16GB","i9","512GB",50103.50));
+	   list.add(new Computer("Applie","16GB","i7","512GB",90403.50));
+	   return list;
    }
 }
