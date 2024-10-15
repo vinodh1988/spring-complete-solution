93999e4ef8999a1e35b8883f9697325f17a156cd
vinodh1988 <k.c.vinodh@hotmail.com>
2024-10-15 13:40:31 +0530

    rest-continued

--- src/main/java/com/springapps/controller/CommonAPI.java
+++ src/main/java/com/springapps/controller/CommonAPI.java
@@ -1,8 +1,14 @@
 package com.springapps.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/common")
 public class CommonAPI {
 
+	@RequestMapping("")
+	public String first() {
+		return "spring boot is running";
+	}
 }
---
+++ src/main/java/com/springapps/entities/Computer.java
@@ -0,0 +1,5 @@
+package com.springapps.entities;
+
+public class Computer {
+
+}
