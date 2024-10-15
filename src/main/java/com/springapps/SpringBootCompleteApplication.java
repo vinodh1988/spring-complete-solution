d072c53249d07cabc3c36b5020b9f82d1fde112f
vinodh1988 <k.c.vinodh@hotmail.com>
2024-10-15 10:51:08 +0530

    first-commit

---
+++ src/main/java/com/springapps/SpringBootCompleteApplication.java
@@ -0,0 +1,13 @@
+package com.springapps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCompleteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCompleteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springapps/SpringBootCompleteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springapps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCompleteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
