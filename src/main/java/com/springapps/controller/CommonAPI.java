cb53bf24e00a3c9d4c43227bb1c1816b7d457bf1
vinodh1988 <k.c.vinodh@hotmail.com>
2024-10-15 13:51:02 +0530

    DataService

--- src/main/java/com/springapps/controller/CommonAPI.java
+++ src/main/java/com/springapps/controller/CommonAPI.java
@@ -1,14 +1,29 @@
 package com.springapps.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.springapps.entities.Computer;
+import com.springapps.services.DataService;
 
 @RestController
 @RequestMapping("/api/common")
 public class CommonAPI {
 
+	@Autowired
+	private DataService dservice;
+	
 	@RequestMapping("")
 	public String first() {
 		return "spring boot is running";
+	}
+	
+
+	@RequestMapping("/computers")
+	public List<Computer> computers() {
+		return dservice.getComputers();
 	}
 }
