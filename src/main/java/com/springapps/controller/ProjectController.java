344df77e9cb2d2d18a67e3e34aff501c3819a5d6
vinodh1988 <k.c.vinodh@hotmail.com>
2024-10-16 12:45:13 +0530

    accessbyid

--- src/main/java/com/springapps/controller/ProjectController.java
+++ src/main/java/com/springapps/controller/ProjectController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.springapps.entities.Project;
 import com.springapps.services.ProjectService;
 import com.springapps.utilities.RecordAlreadyExistsException;
+import com.springapps.utilities.RecordNotFoundException;
 
 @RestController
 @RequestMapping("/api/projects")
@@ -25,6 +27,22 @@ public class ProjectController {
 	@GetMapping("")
 	public List<Project> getProjects() {
 		return pservice.getProjects();
+	}
+	
+	@GetMapping("/{projectno}")
+	public ResponseEntity<Object> getProject(@PathVariable Integer projectno) {
+	 try {
+		return new ResponseEntity<>(pservice.getProject(projectno),HttpStatus.OK);
+	 }
+	 catch(RecordNotFoundException e) {
+		 return new ResponseEntity<>(e.getMessage(),HttpStatus.BAD_REQUEST);
+	 }
+	 
+	 catch(Exception e)
+		{
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	 
 	}
 	//RequestBody would parse json and convert to object
 	@PostMapping("")
