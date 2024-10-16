db30a2bdd8fa868e174f975b180ca08416ca30d4
vinodh1988 <k.c.vinodh@hotmail.com>
2024-10-16 10:38:47 +0530

    pom-changes

--- src/main/java/com/springapps/SpringBootCompleteApplication.java
+++ src/main/java/com/springapps/SpringBootCompleteApplication.java
@@ -2,13 +2,19 @@ package com.springapps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//@ComponentScan(basePackages = "com.appx.solution") to scan the packages which are out of the base package
+// @EntityScan(basePackages = "com.appx.entities")
+
 public class SpringBootCompleteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootCompleteApplication.class, args);
 		System.out.println("App started");
+		
 	}
 
 }
