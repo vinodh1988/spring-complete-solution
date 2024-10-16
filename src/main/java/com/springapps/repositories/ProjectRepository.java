9365bb4f7a1abb2548d4c20d47758df3b3cbf120
vinodh1988 <k.c.vinodh@hotmail.com>
2024-10-16 12:00:35 +0530

    repochaange

--- src/main/java/com/springapps/repositories/ProjectRepository.java
+++ src/main/java/com/springapps/repositories/ProjectRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.springapps.entities.Project;
 
 public interface ProjectRepository extends JpaRepository<Project, Integer> {
-
+   public Project findByProjectno(Integer projectno);
 }
 
 //implementation for this interface is taken care by data jpa itself
