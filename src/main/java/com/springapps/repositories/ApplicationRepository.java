aa5060e81d283cc5640868558a8029644b4e078f
vinodh1988 <k.c.vinodh@hotmail.com>
2024-10-17 11:50:11 +0530

    post-implementation

--- src/main/java/com/springapps/repositories/ApplicationRepository.java
+++ src/main/java/com/springapps/repositories/ApplicationRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.springapps.entities.Application;
 
 public interface ApplicationRepository extends JpaRepository<Application, Integer> {
-
+   public Application getByApplicationno(Integer appicationno);
 }
--- src/main/java/com/springapps/services/ApplicationService.java
+++ src/main/java/com/springapps/services/ApplicationService.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.springapps.entities.Application;
+import com.springapps.entities.Event;
 import com.springapps.repositories.ApplicationRepository;
+import com.springapps.utilities.RecordAlreadyExistsException;
 
 @Service
 public class ApplicationService {
@@ -16,5 +18,18 @@ public class ApplicationService {
 	 
 	 public List<Application> getApplications(){
 		 return arepo.findAll();
+	 }
+	 
+	 public void addApplication(Application application) throws RecordAlreadyExistsException {
+		  Application a= arepo.getByApplicationno(application.getApplicationno());
+		  if(a==null) {
+			  for(Event e: application.getEvents()) {
+				  e.setApplication(application);// we are setting application for event
+				                 //this facilitates auto insertion of applcationno
+			  }
+			  arepo.save(application);
+		  }
+		  else
+			  throw new RecordAlreadyExistsException();
 	 }
 }
