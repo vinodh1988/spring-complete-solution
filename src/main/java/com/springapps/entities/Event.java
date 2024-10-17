ceb16c76b4ba729bedc68427407f1dd40635e8c6
vinodh1988 <k.c.vinodh@hotmail.com>
2024-10-17 10:39:33 +0530

    event-entity

--- src/main/java/com/springapps/entities/Event.java
+++ src/main/java/com/springapps/entities/Event.java
@@ -1,5 +1,47 @@
 package com.springapps.entities;
 
-public class Event {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table
+public class Event {
+@Id
+   private Integer eventno;
+@Column   
+   private String eventname;
+@Column 
+   private String  cause;
+
+public Event() {}
+public Event(Integer eventno, String eventname, String cause) {
+	super();
+	this.eventno = eventno;
+	this.eventname = eventname;
+	this.cause = cause;
+}
+Integer getEventno() {
+	return eventno;
+}
+void setEventno(Integer eventno) {
+	this.eventno = eventno;
+}
+String getEventname() {
+	return eventname;
+}
+void setEventname(String eventname) {
+	this.eventname = eventname;
+}
+String getCause() {
+	return cause;
+}
+void setCause(String cause) {
+	this.cause = cause;
+}
+  
+
+
+   
 }
